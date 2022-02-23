cc6e23efd855300e92554101cc9955f52c8e2f53
Igor Santos <global.igorsantosweb@gmail.com>
2022-02-23 13:54:21 -0300

    create menu mobile function control

--- 01-wireframe/version01/js/script.js
+++ 01-wireframe/version01/js/script.js
@@ -1,5 +1,14 @@
 function handleClickBtnMobile() {
-  console.log('Hello World');
+  const btn = document.querySelector('[data-menu="btn"]');
+  const nav = document.querySelector('[data-menu="nav"]');
+
+  function toggleMenuMobile() {
+    nav.classList.toggle('active');
+  }
+
+  if (btn && nav) {
+    btn.addEventListener('click', toggleMenuMobile);
+  }
 }
 
 handleClickBtnMobile();
