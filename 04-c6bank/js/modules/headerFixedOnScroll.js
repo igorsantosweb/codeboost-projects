321831238debce64e23642118b913fdf1d19a1fb
Igor Santos <global.igorsantosweb@gmail.com>
2022-04-09 23:42:21 -0300

    create header fixed on scroll effect

--- 04-c6bank/js/modules/headerFixedOnScroll.js
+++ 04-c6bank/js/modules/headerFixedOnScroll.js
@@ -1,5 +1,15 @@
 const headerFixedOnScroll = () => {
-  console.log('--- Hello World');
+  const header = document.querySelector('[data-header]');
+  
+  function handleScroll() {
+    if(window.pageYOffset > 110) {
+      header.classList.add('active');
+    } else {
+      header.classList.remove('active');
+    }
+  }
+
+  window.addEventListener('scroll', handleScroll);
 }
 
 export default headerFixedOnScroll;
