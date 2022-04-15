0a0d3d41f2e0ef3f03d4b2b8d41fc4c36918febf
Igor Santos <global.igorsantosweb@gmail.com>
2022-04-15 18:45:13 -0300

    update headerFixedOnScroll function

--- 04-c6bank/js/modules/headerFixedOnScroll.js
+++ 04-c6bank/js/modules/headerFixedOnScroll.js
@@ -1,11 +1,26 @@
 const headerFixedOnScroll = () => {
   const header = document.querySelector('[data-header]');
+  let isAlreadySubmenusDisables = false;
   
   function handleScroll() {
+    const submenus = document.querySelectorAll('[data-dropdown="menu"]');
+
     if(window.pageYOffset > 110) {
       header.classList.add('active');
+
+      // Disable all submenus acitve 
+      if(!isAlreadySubmenusDisables) {
+        submenus.forEach((submenu) => {
+          submenu.classList.remove('active');
+        });
+
+        isAlreadySubmenusDisables = true;
+      }
+
     } else {
       header.classList.remove('active');
+
+      isAlreadySubmenusDisables = false;
     }
   }
 
