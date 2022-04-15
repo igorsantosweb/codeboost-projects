e259c432c7063a16d7d45b341b39b655d9114162
Igor Santos <global.igorsantosweb@gmail.com>
2022-04-15 18:44:41 -0300

    create toggleMenuDropdown function

---
+++ 04-c6bank/js/modules/toggleMenuDropdown.js
@@ -0,0 +1,22 @@
+const toggleMenuDropdown = () => {
+  const links = document.querySelectorAll('[data-dropdown="link"]');
+  const submenus = document.querySelectorAll('[data-dropdown="menu"]');
+
+  function toggleSubmenu(indexTarget) {
+    submenus[indexTarget].classList.toggle('active');
+  }
+
+  links.forEach((link, index) => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault();
+
+      toggleSubmenu(index);
+    });
+  });
+
+  submenus.forEach((submenu, index) => {
+    submenu.addEventListener('mouseleave', () => toggleSubmenu(index));
+  })
+}
+
+export default toggleMenuDropdown;
--- 04-c6bank/js/script.js
+++ 04-c6bank/js/script.js
@@ -1,5 +1,7 @@
 import initHeaderFixedOnScroll from './modules/headerFixedOnScroll.js';
 import initHandleScrollWindow from './modules/scrollAnimation.js';
+import initToggleMenuDropdown from './modules/toggleMenuDropdown.js';
 
 initHeaderFixedOnScroll();
 initHandleScrollWindow();
+initToggleMenuDropdown();
